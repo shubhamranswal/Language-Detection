20266843a6e46bb4f554d43390f47cbf3f05ce05
Shubham Singh Ranswal <shubhamranswal@gmail.com>
2021-09-05 17:10:10 +0530

    initial commit

---
+++ app/src/main/java/com/shubhamranswal/languagedetection/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.shubhamranswal.languagedetection
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mlkit.nl.languageid.LanguageIdentification
+import com.google.mlkit.nl.languageid.LanguageIdentifier
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var etLangText:EditText
+    lateinit var btnCheckNow:Button
+    lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etLangText = findViewById(R.id.etLangText)
+        btnCheckNow = findViewById(R.id.btnCheckNow)
+        tvResult = findViewById(R.id.tvResult)
+
+        btnCheckNow.setOnClickListener{
+            val langText:String = etLangText.text.toString()
+            if (langText == ""){
+                //toast for entering input
+            }else{
+                detectLang(langText)
+            }
+        }
+    }
+
+    private fun detectLang(langText: String) {
+        val languageIdentifier:LanguageIdentifier = LanguageIdentification.getClient()
+
+        languageIdentifier.identifyLanguage(langText)
+            .addOnSuccessListener { languageCode ->
+                if (languageCode == "und") {
+                    tvResult.text = "Can't identify language."
+                } else {
+                    tvResult.text = "Language: $languageCode"
+                }
+            }
+            .addOnFailureListener {
+                tvResult.text = "Can't detect it. Reason ${it.message}"
+            }
+
+
+    }
+}
